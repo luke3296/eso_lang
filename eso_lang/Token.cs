1a8f3be8320cdde211d9af7f537416ae09e18adf
Luke A <sluke2210@gmail.com>
2022-10-14 17:32:42 +0100

    fixed bug in Token class

--- eso_lang/Token.cs
+++ eso_lang/Token.cs
@@ -19,22 +19,34 @@ namespace eso_lang
             this.name = name;
             this.tokenRE = new Regex(restr);
         }
+
         public Token(int id, string name, Regex tok_re)
         {
             this.id = id;
             this.name = name;
             this.tokenRE = tok_re;
         }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public Regex RE { get; set; }
+
         public bool match(string lexeme) {
-            return this.tokenRE.IsMatch(lexeme);
-            //if (tokenRE.IsMatch(lexeme) != null) {
-             //   Console.WriteLine("matched" + lexeme);
-              //  return true;
-           // }
-           // return false;
+        MatchCollection matches =this.tokenRE.Matches(lexeme);
+        List<string> match_list = new List<string>();
+        foreach(Match match in matches)
+        {
+         match_list.Add(match.Value);
+        }
+        if(match_list.Count > 1){
+            Console.WriteLine("more than on match for regex {0}" , this.tokenRE);
+            return false;
+        }else if(match_list.Count == 0){
+            return false;
+        }else{
+            return true;
+        }
+            
         }
 
     }
